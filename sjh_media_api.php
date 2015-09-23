879e8b07f0eb684085816adec5398936751cde1e
SJH Media <aran+sjh@evosite.co.uk>
2015-09-23 16:40:45 +0100

    Updated to API version 2.00 and fixed a bug where the auto-generated nonce value wasn't inserted unless you were adding an extra parameter of your own to the requests.

--- sjh_media_api.php
+++ sjh_media_api.php
@@ -1,251 +1,252 @@
 <?php
+
 /**
  * Class sjh_media
  */
 final class sjh_media {
 
-	// TODO; Add your own API credentials here
-	const API_KEY    = 'YOUR_API_KEY_HERE';
-	const SECRET_KEY = 'YOUR_SECRET_KEY_HERE';
-	// TODO; End of settings to be changed - Don't edit beneath here
+    // TODO; Add your own API credentials here
+    const API_KEY = 'YOUR_API_KEY_HERE';
+    const SECRET_KEY = 'YOUR_SECRET_KEY_HERE';
+    // TODO; End of settings to be changed - Don't edit beneath here
 
-	const API_VERSION = '1.0';
-	const API_HOSTNAME = 'https://www.sjhmedia.net';
+    const API_VERSION = '2.00';
+    const API_HOSTNAME = 'https://www.sjhmedia.net';
 
-	private $api_params = [
-		'api_key' => self::API_KEY
-	];
-	private $api_endpoint = null;
-	private $api_endpoint_details = null;
+    private $api_params = [
+        'api_key' => self::API_KEY
+    ];
+    private $api_endpoint = null;
+    private $api_endpoint_details = null;
 
-	private $api_endpoints = [
-		'feed' => [
-			'method' => 'GET',
-			'required_parameters' => [],
-		],
-		'status' => [
-			'method' => 'GET',
-			'required_parameters' => [
-				'id',
-			],
-		],
-		'update' => [
-			'method' => 'POST',
-			'required_parameters' => [
-				'id',
-				'action',
-			],
-		],
-		'recycle' => [
-			'method' => 'POST',
-			'required_parameters' => [
-				'gadgets',
-				'first_name',
-				'last_name',
-				'email_address',
-				'phone_number',
-				'address1',
-				'town',
-				'county',
-				'postcode',
-				'country',
-				'ip_address',
-				'payment_method',
-				'number_of_labels',
-			],
-		]
-	];
+    private $api_endpoints = [
+        'feed' => [
+            'method' => 'GET',
+            'required_parameters' => [],
+        ],
+        'status' => [
+            'method' => 'GET',
+            'required_parameters' => [
+                'id',
+            ],
+        ],
+        'update' => [
+            'method' => 'POST',
+            'required_parameters' => [
+                'id',
+                'action',
+            ],
+        ],
+        'recycle' => [
+            'method' => 'POST',
+            'required_parameters' => [
+                'gadgets',
+                'first_name',
+                'last_name',
+                'email_address',
+                'phone_number',
+                'address1',
+                'town',
+                'county',
+                'postcode',
+                'country',
+                'ip_address',
+                'payment_method',
+                'number_of_labels',
+            ],
+        ]
+    ];
 
-	/**
-	 * @param string $api_endpoint
-	 * @return bool
-	 * @throws \Exception
-	 */
-	public function setApiEndpoint($api_endpoint) {
-		$api_endpoint = strtolower(trim($api_endpoint, '/\\ '));
-		if (isset($this->api_endpoints[$api_endpoint])) {
-			// Store the API endpoint details
-			$this->api_endpoint_details = $this->api_endpoints[$api_endpoint];
-			$this->api_endpoint = $api_endpoint;
+    /**
+     * @param string $api_endpoint
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function setApiEndpoint($api_endpoint) {
+        $api_endpoint = strtolower(trim($api_endpoint, '/\\ '));
+        if (isset($this->api_endpoints[$api_endpoint])) {
+            // Store the API endpoint details
+            $this->api_endpoint_details = $this->api_endpoints[$api_endpoint];
+            $this->api_endpoint = $api_endpoint;
 
-			return true;
-		}
+            return true;
+        }
 
-		throw new Exception('Please ensure you\'re specifying a valid endpoint for this API/SDK version');
-	}
+        throw new Exception('Please ensure you\'re specifying a valid endpoint for this API/SDK version');
+    }
 
-	/**
-	 * @return string
-	 * @throws \Exception
-	 */
-	public function doApiRequest() {
-		try {
-			$this->doVerifyRequest();
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    public function doApiRequest() {
+        try {
+            $this->doVerifyRequest();
 
-			$request_method = $this->getRequestMethod();
-			$curl_options = [
-				CURLOPT_URL => $this->getEndpointUrl(),
-				CURLOPT_RETURNTRANSFER => 1,
-				CURLOPT_SSL_VERIFYPEER => false,
-				CURLOPT_USERAGENT => 'SJH Media SDK v' . self::API_VERSION,
-				CURLOPT_HTTPHEADER => [
-					'x-sjh-media-signature: ' . $this->getRequsetSignature(),
-					'x-sjh-media-referrer: ' . $this->getReferrer()
-				]
-			];
+            $request_method = $this->getRequestMethod();
+            $curl_options = [
+                CURLOPT_URL => $this->getEndpointUrl(),
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_USERAGENT => 'SJH Media SDK v' . self::API_VERSION,
+                CURLOPT_HTTPHEADER => [
+                    'x-sjh-media-signature: ' . $this->getRequsetSignature(),
+                    'x-sjh-media-referrer: ' . $this->getReferrer()
+                ]
+            ];
 
-			$request_parameters = $this->getSortedParameters();
+            $request_parameters = $this->getSortedParameters();
 
-			if ($request_method === 'POST') {
-				$curl_options[CURLOPT_POST] = true;
-				$curl_options[CURLOPT_POSTFIELDS] = http_build_query($request_parameters);
-			} else if ($request_method === 'GET') {
-				$curl_options[CURLOPT_URL] .= '?' . http_build_query($request_parameters);
-			}
+            if ($request_method === 'POST') {
+                $curl_options[CURLOPT_POST] = true;
+                $curl_options[CURLOPT_POSTFIELDS] = http_build_query($request_parameters);
+            } else if ($request_method === 'GET') {
+                $curl_options[CURLOPT_URL] .= '?' . http_build_query($request_parameters);
+            }
 
-			$curl = curl_init();
-			curl_setopt_array($curl, $curl_options);
-			$api_response = curl_exec($curl);
-			if ($api_response === false) {
-				$response_headers = curl_getinfo($curl);
-				echo '<p><pre>' . print_r($response_headers, true) . '</pre></p>'."\n";
-			}
-			curl_close($curl);
+            $curl = curl_init();
+            curl_setopt_array($curl, $curl_options);
+            $api_response = curl_exec($curl);
+            if ($api_response === false) {
+                $response_headers = curl_getinfo($curl);
+                echo '<p><pre>' . print_r($response_headers, true) . '</pre></p>' . "\n";
+            }
+            curl_close($curl);
 
-			return $api_response;
+            return $api_response;
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+    }
 
-		} catch(Exception $e) {
-			throw new Exception($e);
-		}
-	}
+    /**
+     * @return bool
+     * @throws \Exception
+     */
+    private function doVerifyRequest() {
+        $errors = [];
 
-	/**
-	 * @return bool
-	 */
-	private function isHttps() {
-		if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
-			return true;
-		}
+        if ($this->api_endpoint === null || $this->api_endpoint_details === null) {
+            $errors[] = 'Please make sure to call $this->setApiEndpoint("/change_to_your_required_feature") before calling $this->doApiRequest()';
+        }
 
-		if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-			// Fix for proxies / load balancers which oftern strip the HTTPS headers before routing
-			return true;
-		}
+        if (!isset($this->api_params['nonce'])) {
+            $this->api_params['nonce'] = $this->getNonceValue();
+        }
+        if (!empty($this->api_endpoint_details['required_parameters'])) {
+            $missing_parameters = [];
+            foreach ($this->api_endpoint_details['required_parameters'] as $required_param) {
+                if (!isset($this->api_params[$required_param])) {
+                    $missing_parameters[] = $required_param;
+                }
+            }
 
-		return false;
-	}
+            if (!empty($missing_parameters)) {
+                $errors[] = 'The following required parameters are missing from your API request: ' . implode(', ', $missing_parameters);
+            }
+        }
 
-	/**
-	 * @return null|string
-	 */
-	private function getReferrer() {
-		if (isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST']) && isset($_SERVER['REQUEST_URI'])) {
-			return 'http' . ($this->isHttps() ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		}
+        if (!empty($errors)) {
+            throw new Exception('API request failed to pass checks with the following errors: ' . implode(', ', $errors));
+        }
 
-		return null;
-	}
+        return true;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function setRequestParameter( /* Polymorphic - Can be $key, $value OR array('key1' => 'val1'...) */ ) {
-		$arguments = func_get_args();
-		if (count($arguments) == 2) {
-			// Handle $key, $value arguments
-			$this->api_params[$arguments[0]] = $arguments[1];
-		} else if (is_array($arguments[0])) {
-			// Handle arrays arguments
-			foreach ($arguments[0] as $key => $value) {
-				$this->api_params[$key] = $value;
-			}
-		} else {
-			throw new InvalidArgumentException('$this->setRequestParameter() is Polymorphic - Parameters can be formatted as ($key, $value) OR (array("key1" => "val1", ...))');
-		}
+    /**
+     * @return string
+     */
+    private function getRequestMethod() {
+        return strtoupper($this->api_endpoint_details['method']);
+    }
 
-		if (!isset($this->api_params['nonce'])) {
-			$this->api_params['nonce'] = $this->getNonceValue();
-		}
+    /**
+     * @return string
+     */
+    private function getEndpointUrl() {
+        return self::API_HOSTNAME . '/api/' . self::API_VERSION . '/' . $this->api_endpoint;
+    }
 
-		return true;
-	}
+    /**
+     * @return string
+     */
+    private function getRequsetSignature() {
+        return hash_hmac('sha512', $this->getFullRequestUrl(), self::SECRET_KEY);
+    }
 
-	/**
-	 * @return int
-	 */
-	private function getNonceValue() {
-		return time(); // By default this will suffice
-	}
+    /**
+     * @return string
+     */
+    private function getFullRequestUrl() {
+        $url = $this->getEndpointUrl();
+        if (!empty($this->api_params)) {
+            $url .= '?' . http_build_query($this->getSortedParameters());
+        }
 
-	/**
-	 * @return string
-	 */
-	private function getRequsetSignature() {
-		return hash_hmac('sha512', $this->getFullRequestUrl(), self::SECRET_KEY);
-	}
+        return $url;
+    }
 
-	/**
-	 * @return string
-	 */
-	private function getFullRequestUrl() {
-		$url = $this->getEndpointUrl();
-		if (!empty($this->api_params)) {
-			$url .= '?' . http_build_query($this->getSortedParameters());
-		}
+    /**
+     * @return array
+     */
+    private function getSortedParameters() {
+        ksort($this->api_params); // Sort parameters alphabetically by their key
 
-		return $url;
-	}
+        return $this->api_params;
+    }
 
-	/**
-	 * @return string
-	 */
-	private function getEndpointUrl() {
-		return self::API_HOSTNAME . '/api/' . self::API_VERSION . '/' . $this->api_endpoint;
-	}
+    /**
+     * @return null|string
+     */
+    private function getReferrer() {
+        if (isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST']) && isset($_SERVER['REQUEST_URI'])) {
+            return 'http' . ($this->isHttps() ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        }
 
-	/**
-	 * @return array
-	 */
-	private function getSortedParameters() {
-		ksort($this->api_params); // Sort parameters alphabetically by their key
+        return null;
+    }
 
-		return $this->api_params;
-	}
+    /**
+     * @return bool
+     */
+    private function isHttps() {
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+            return true;
+        }
 
-	/**
-	 * @return string
-	 */
-	private function getRequestMethod() {
-		return strtoupper($this->api_endpoint_details['method']);
-	}
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            // Fix for proxies / load balancers which oftern strip the HTTPS headers before routing
+            return true;
+        }
 
-	/**
-	 * @return bool
-	 * @throws \Exception
-	 */
-	private function doVerifyRequest() {
-		$errors = [];
+        return false;
+    }
 
-		if ($this->api_endpoint === null || $this->api_endpoint_details === null) {
-			$errors[] = 'Please make sure to call $this->setApiEndpoint("/change_to_your_required_feature") before calling $this->doApiRequest()';
-		}
-		if (!empty($this->api_endpoint_details['required_parameters'])) {
-			$missing_parameters = [];
-			foreach ($this->api_endpoint_details['required_parameters'] as $required_param) {
-				if (!isset($this->api_params[$required_param])) {
-					$missing_parameters[] = $required_param;
-				}
-			}
+    /**
+     * @return bool
+     */
+    public function setRequestParameter( /* Polymorphic - Can be $key, $value OR array('key1' => 'val1'...) */) {
+        $arguments = func_get_args();
+        if (count($arguments) == 2) {
+            // Handle $key, $value arguments
+            $this->api_params[$arguments[0]] = $arguments[1];
+        } else if (is_array($arguments[0])) {
+            // Handle arrays arguments
+            foreach ($arguments[0] as $key => $value) {
+                $this->api_params[$key] = $value;
+            }
+        } else {
+            throw new InvalidArgumentException('$this->setRequestParameter() is Polymorphic - Parameters can be formatted as ($key, $value) OR (array("key1" => "val1", ...))');
+        }
 
-			if (!empty($missing_parameters)) {
-				$errors[] = 'The following required parameters are missing from your API request: ' . implode(', ', $missing_parameters);
-			}
-		}
+        return true;
+    }
 
-		if (!empty($errors)) {
-			throw new Exception('API request failed to pass checks with the following errors: ' . implode(', ', $errors));
-		}
-
-		return true;
-	}
+    /**
+     * @return int
+     */
+    private function getNonceValue() {
+        return time(); // By default this will suffice
+    }
 }
